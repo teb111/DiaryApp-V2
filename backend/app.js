badfccf7d8dcb38b5cddf2840fdcd592aa28125f
teb111 <fakoredetobiloba.ft@gmail.com>
2023-06-01 16:53:14 +0100

    m2

--- backend/app.js
+++ backend/app.js
@@ -1,49 +1,49 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import colors from "colors";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import diaryRoutes from "./routes/diaryRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+import path from "path"
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import colors from "colors"
+import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import diaryRoutes from "./routes/diaryRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 //load config
-dotenv.config({});
+dotenv.config({})
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev"))
 }
 
-app.use(express.json());
+app.use(express.json())
 
 // routes
-app.use("/auth/google", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/diary", diaryRoutes);
-app.use("/api/uploads", uploadRoutes);
+app.use("/auth/google", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/diary", diaryRoutes)
+app.use("/api/uploads", uploadRoutes)
 
 // images
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("This is working");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("This is working");
+//   });
+// }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`App running on port 5000`));
+app.listen(PORT, console.log(`App running on port 5000`))
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,53 +1,53 @@
-import asyncHandler from "express-async-handler";
-import generateToken from "../utils/generateToken.js";
-import mongoose from "mongoose";
-import User from "../models/User.js";
-import Diary from "../models/Diary.js";
-import { OAuth2Client } from "google-auth-library";
+import asyncHandler from "express-async-handler"
+import generateToken from "../utils/generateToken.js"
+import mongoose from "mongoose"
+import User from "../models/User.js"
+import Diary from "../models/Diary.js"
+import { OAuth2Client } from "google-auth-library"
 
 const client = new OAuth2Client(
   "247079180053-gjglmnlaevngo30bgmibj4olgdhv22qm.apps.googleusercontent.com"
-);
+)
 
 // @desc     Register a new User
 // @route    POST /api/users
 // @access   Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, image } = req.body;
+  const { name, email, password, image } = req.body
 
   // cheecking if the user already exists in the database
-  const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email })
 
   if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
+    res.status(400)
+    throw new Error("User already exists")
   }
   // if not we go ahead and create the new user
   const user = await User.create({
     name,
     email,
     password,
-    image,
-  });
+    image
+  })
 
   if (user) {
-    res.status(201);
+    res.status(201)
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
       image: user.image,
-      token: generateToken(user._id), // generating a token alongside the user's id
-    });
+      token: generateToken(user._id) // generating a token alongside the user's id
+    })
   } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+    res.status(400)
+    throw new Error("Invalid user data")
   }
-});
+})
 
 const loginWithGoogle = asyncHandler((req, res) => {
-  const { tokenId } = req.body;
+  const { tokenId } = req.body
 
   // verifying if the token from the client and the backend is the same
 
@@ -55,16 +55,16 @@ const loginWithGoogle = asyncHandler((req, res) => {
     const result = await client.verifyIdToken({
       idToken: tokenId,
       audience:
-        "247079180053-gjglmnlaevngo30bgmibj4olgdhv22qm.apps.googleusercontent.com",
-    });
+        "247079180053-gjglmnlaevngo30bgmibj4olgdhv22qm.apps.googleusercontent.com"
+    })
 
-    const { email_verified, name, email } = result.payload;
+    const { email_verified, name, email } = result.payload
 
     if (email_verified) {
-      const userExists = await User.findOne({ email });
+      const userExists = await User.findOne({ email })
       if (userExists) {
         try {
-          res.status(200);
+          res.status(200)
           res.json({
             _id: userExists._id,
             name: userExists.name,
@@ -74,43 +74,45 @@ const loginWithGoogle = asyncHandler((req, res) => {
             twitter: userExists.twitter,
             instagram: userExists.instagram,
             image: userExists.image,
-            token: generateToken(userExists._id), // generating a token alongside the user's id
-          });
+            token: generateToken(userExists._id) // generating a token alongside the user's id
+          })
         } catch (err) {
-          res.status(400);
-          throw new Error("User not found");
+          res.status(400)
+          throw new Error("User not found")
         }
       } else {
-        const password = `${email}${process.env.JWT_SECRET}`;
+        const password = `${email}${process.env.JWT_SECRET}`
         const user = await User.create({
           name,
           email,
-          password,
-        });
+          password
+        })
 
         if (user) {
-          res.status(201);
+          res.status(201)
           res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: generateToken(user._id), // generating a token alongside the user's id
-          });
+            token: generateToken(user._id) // generating a token alongside the user's id
+          })
         } else {
-          res.status(400);
-          throw new Error("Sorry, Something Went Wrong");
+          res.status(400)
+          throw new Error("Sorry, Something Went Wrong")
         }
       }
     }
-  };
+  }
 
-  verified();
-});
+  verified()
+})
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const user = await User.findOne({ email });
+  console.log(email, password)
+
+  const user = await User.findOne({ email })
 
   // checking to see if the password the user entered correlates with the one in the database with our method from userModel
   if (user && (await user.matchPassword(password))) {
@@ -123,32 +125,32 @@ const loginUser = asyncHandler(async (req, res) => {
       linkedIn: user.linkedIn,
       twitter: user.twitter,
       instagram: user.instagram,
-      token: generateToken(user._id), // generating a token alongside the user's id
-    });
+      token: generateToken(user._id) // generating a token alongside the user's id
+    })
   } else {
-    res.status(401).json({ message: "Invalid Email or password" });
+    res.status(401).json({ message: "Invalid Email or password" })
   }
-});
+})
 
 // Edit User
 // route PUT /api/user/:id
 //access private
 
 const editUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-  const { name, email, bio, linkedIn, twitter, instagram, image } = req.body;
+  const user = await User.findById(req.user._id)
+  const { name, email, bio, linkedIn, twitter, instagram, image } = req.body
   if (user) {
-    user.name = name || user.name;
-    user.email = email || user.email;
-    user.bio = bio || user.bio;
-    user.linkedIn = linkedIn || user.linkedIn;
-    user.twitter = twitter || user.twitter;
-    user.instagram = instagram || user.instagram;
-    user.image = image || user.image;
+    user.name = name || user.name
+    user.email = email || user.email
+    user.bio = bio || user.bio
+    user.linkedIn = linkedIn || user.linkedIn
+    user.twitter = twitter || user.twitter
+    user.instagram = instagram || user.instagram
+    user.image = image || user.image
     if (req.body.password) {
-      user.password = req.body.password;
+      user.password = req.body.password
     }
-    const updatedUser = await user.save();
+    const updatedUser = await user.save()
 
     res.json({
       _id: updatedUser._id,
@@ -158,43 +160,43 @@ const editUser = asyncHandler(async (req, res) => {
       twitter: updatedUser.twitter,
       instagram: updatedUser.instagram,
       image: updatedUser.image,
-      token: generateToken(updatedUser._id),
-    });
+      token: generateToken(updatedUser._id)
+    })
   }
-});
+})
 
 // Create Bookmarks
 // route POST /api/user/:id/bookmark
 //access private
 
 const createBookmark = asyncHandler(async (req, res) => {
-  const { id } = req.body;
-  const userId = req.params.id;
-  const user = await User.findById(req.params.id);
+  const { id } = req.body
+  const userId = req.params.id
+  const user = await User.findById(req.params.id)
 
   if (user) {
     const bookmark = {
-      diary: id,
-    };
+      diary: id
+    }
 
     const alreadyBookmarked = user.bookmarks.find(
       (r) => r.diary.toString() === id.toString()
-    );
+    )
 
     if (alreadyBookmarked) {
-      deleteBookmark(userId, user, alreadyBookmarked);
-      res.status(200).json({ message: "Boomarked removed" });
+      deleteBookmark(userId, user, alreadyBookmarked)
+      res.status(200).json({ message: "Boomarked removed" })
     } else {
-      user.bookmarks.push(bookmark);
-      await user.save();
+      user.bookmarks.push(bookmark)
+      await user.save()
 
-      res.status(201).json({ message: "Post Bookmarked" });
+      res.status(201).json({ message: "Post Bookmarked" })
     }
   } else {
-    res.status(500).res.json({ message: "An Error Occured, Please Try Again" });
-    throw new Error("Post was not bookmarked");
+    res.status(500).res.json({ message: "An Error Occured, Please Try Again" })
+    throw new Error("Post was not bookmarked")
   }
-});
+})
 
 // Delete bookmark
 // route Create Bookmark calls it
@@ -206,56 +208,56 @@ const deleteBookmark = asyncHandler(
       const getBookmarkbyId = await User.updateOne(
         { _id: userId },
         { $pull: { bookmarks: { _id: alreadyBookmarked._id } } }
-      );
+      )
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
-);
+)
 
 // Get Bookmarks
 // route GET /api/user/:id/bookmark
 //access private
 
 const getBookmarks = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
   if (user) {
     const bookmarks = user.bookmarks.map((book) => {
-      return book.diary.toString();
-    });
+      return book.diary.toString()
+    })
 
-    let books = [];
+    let books = []
 
     const diaries = bookmarks.forEach(function (book) {
-      books.push(mongoose.Types.ObjectId(String(book)));
-    });
+      books.push(mongoose.Types.ObjectId(String(book)))
+    })
 
     const bookmark = await Diary.find(
       {
         _id: {
-          $in: books,
-        },
+          $in: books
+        }
       },
       function (err, docs) {
         if (err) {
           res.status(404).res.json({
             message:
-              "An Error Occured while trying to bookmark this post, Please Try Again",
-          });
+              "An Error Occured while trying to bookmark this post, Please Try Again"
+          })
         }
       }
-    ).populate("user name email");
-    res.json(bookmark);
+    ).populate("user name email")
+    res.json(bookmark)
   } else {
     res.status(500).res.json({
-      message: "An Error Occured, Please Log in Again",
-    });
-    throw new Error("User not found");
+      message: "An Error Occured, Please Log in Again"
+    })
+    throw new Error("User not found")
   }
-});
+})
 
 const getUserDetails = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
 
   if (user) {
     res.json({
@@ -266,12 +268,12 @@ const getUserDetails = asyncHandler(async (req, res) => {
       image: user.image,
       linkedIn: user.linkedIn,
       twitter: user.twitter,
-      instagram: user.instagram,
-    });
+      instagram: user.instagram
+    })
   } else {
-    console.log("An error occured");
+    console.log("An error occured")
   }
-});
+})
 
 export {
   registerUser,
@@ -280,5 +282,5 @@ export {
   editUser,
   createBookmark,
   getBookmarks,
-  getUserDetails,
-};
+  getUserDetails
+}
