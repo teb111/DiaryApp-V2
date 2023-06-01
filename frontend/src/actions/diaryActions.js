5dacab074dba857db95b504aaca8101189210fc8
teb111 <fakoredetobiloba.ft@gmail.com>
2023-06-01 18:23:30 +0100

    m2

--- frontend/src/actions/diaryActions.js
+++ frontend/src/actions/diaryActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios"
 import {
   CREATE_DIARY_REQUEST,
   CREATE_DIARY_SUCCESS,
@@ -17,184 +17,200 @@ import {
   DIARY_CREATE_REVIEW_FAIL,
   GET_USER_DIARY_REQUEST,
   GET_USER_DIARY_SUCCESS,
-  GET_USER_DIARY_FAIL,
-} from "../constants/diaryConstant.js";
+  GET_USER_DIARY_FAIL
+} from "../constants/diaryConstant.js"
 
 export const createDiary = () => async (dispatch, getState) => {
   try {
-    dispatch({ type: CREATE_DIARY_REQUEST });
+    dispatch({ type: CREATE_DIARY_REQUEST })
 
     const {
       userLogin: { userInfo },
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
-    const { data } = await axios.post("/api/diary", {}, config);
+    const { data } = await axios.post(
+      "https://diary-liv9.onrender.com/api/diary",
+      {},
+      config
+    )
 
-    dispatch({ type: CREATE_DIARY_SUCCESS, payload: data });
+    dispatch({ type: CREATE_DIARY_SUCCESS, payload: data })
   } catch (error) {
     dispatch({
       type: CREATE_DIARY_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const updateDiary = (diary) => async (dispatch, getState) => {
   try {
-    dispatch({ type: DIARY_UPDATE_REQUEST });
+    dispatch({ type: DIARY_UPDATE_REQUEST })
 
     const {
       userLogin: { userInfo },
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
-    const { data } = await axios.put(`/api/diary/${diary._id}`, diary, config);
-    dispatch({ type: DIARY_UPDATE_SUCCESS, payload: data });
+    const { data } = await axios.put(
+      `https://diary-liv9.onrender.com/api/diary/${diary._id}`,
+      diary,
+      config
+    )
+    dispatch({ type: DIARY_UPDATE_SUCCESS, payload: data })
   } catch (error) {
     dispatch({
       type: DIARY_UPDATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const listDiary =
   (pageNumber = "", keyword = "") =>
   async (dispatch, getState) => {
     try {
-      dispatch({ type: DIARY_LIST_REQUEST });
+      dispatch({ type: DIARY_LIST_REQUEST })
 
       const { data } = await axios.get(
-        `/api/diary?pageNumber=${pageNumber}&keyword=${keyword}`
-      );
-      dispatch({ type: DIARY_LIST_SUCCESS, payload: data });
+        `https://diary-liv9.onrender.com/api/diary?pageNumber=${pageNumber}&keyword=${keyword}`
+      )
+      dispatch({ type: DIARY_LIST_SUCCESS, payload: data })
     } catch (error) {
       dispatch({
         type: DIARY_LIST_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
-            : error.message,
-      });
+            : error.message
+      })
     }
-  };
+  }
 
 export const getDiaryById = (id) => async (dispatch, getState) => {
   try {
-    dispatch({ type: DIARY_LIST_ID_REQUEST });
+    dispatch({ type: DIARY_LIST_ID_REQUEST })
 
     const {
       userLogin: { userInfo },
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
-    const { data } = await axios.get(`/api/diary/${id}`);
-    dispatch({ type: DIARY_LIST_ID_SUCCESS, payload: data });
+    const { data } = await axios.get(
+      `https://diary-liv9.onrender.com/api/diary/${id}`
+    )
+    dispatch({ type: DIARY_LIST_ID_SUCCESS, payload: data })
   } catch (error) {
     dispatch({
       type: DIARY_LIST_ID_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const createDiaryReview =
   (diaryId, review) => async (dispatch, getState) => {
     try {
-      dispatch({ type: DIARY_CREATE_REVIEW_REQUEST }); // calls the DIARY_UPDATE_REQUEST reducer
+      dispatch({ type: DIARY_CREATE_REVIEW_REQUEST }) // calls the DIARY_UPDATE_REQUEST reducer
 
       const {
         userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-        userLoginGoogle: { userGoogle },
-      } = getState();
+        userLoginGoogle: { userGoogle }
+      } = getState()
 
       const config = {
         headers: {
           Authorization: `Bearer ${
             (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-          }`,
-        },
-      };
+          }`
+        }
+      }
 
-      await axios.post(`/api/diary/${diaryId}/reviews`, review, config);
+      await axios.post(
+        `https://diary-liv9.onrender.com/api/diary/${diaryId}/reviews`,
+        review,
+        config
+      )
 
       dispatch({
-        type: DIARY_CREATE_REVIEW_SUCCESS,
-      });
+        type: DIARY_CREATE_REVIEW_SUCCESS
+      })
     } catch (error) {
       dispatch({
         type: DIARY_CREATE_REVIEW_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
-            : error.message,
-      }); // calls the DIARY_CREATE_REVIEW_FAIL reducer
+            : error.message
+      }) // calls the DIARY_CREATE_REVIEW_FAIL reducer
     }
-  };
+  }
 
 export const getUserDiary = (user) => async (dispatch, getState) => {
   try {
-    dispatch({ type: GET_USER_DIARY_REQUEST });
+    dispatch({ type: GET_USER_DIARY_REQUEST })
 
     const {
       userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
     const userId = `${
       (userGoogle && userGoogle._id) || (userInfo && userInfo._id)
-    }`;
+    }`
 
     const { data } = await axios.get(
-      `/api/diary/${user ? user.userId : userId}/user`,
+      `https://diary-liv9.onrender.com/api/diary/${
+        user ? user.userId : userId
+      }/user`,
       config
-    );
+    )
 
     dispatch({
       type: GET_USER_DIARY_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
   } catch (error) {
     dispatch({
       type: GET_USER_DIARY_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from "axios"
 import {
   CREATE_DIARY_RESET,
-  GET_USER_DIARY_FAIL,
-} from "../constants/diaryConstant";
+  GET_USER_DIARY_FAIL
+} from "../constants/diaryConstant"
 
 import {
   USER_LOGIN_FAIL,
@@ -27,255 +27,273 @@ import {
   USER_EDIT_SUCCESS,
   GET_USER_DETAILS_REQUEST,
   GET_USER_DETAILS_SUCCESS,
-  GET_USER_DETAILS_FAIL,
-} from "../constants/userConstant";
+  GET_USER_DETAILS_FAIL
+} from "../constants/userConstant"
 
 export const loginWithGoogle = (tokenId) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_LOGIN_GOOGLE_REQUEST,
-    });
+      type: USER_LOGIN_GOOGLE_REQUEST
+    })
 
     const config = {
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
+        "Content-Type": "application/json"
+      }
+    }
 
     // getting our response as we set it in our userController in the backend
-    const { data } = await axios.post("/auth/google", { tokenId }, config);
+    const { data } = await axios.post(
+      "https://diary-liv9.onrender.com/auth/google",
+      { tokenId },
+      config
+    )
 
     dispatch({
       type: USER_LOGIN_GOOGLE_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
 
-    localStorage.setItem("userGoogle", JSON.stringify(data));
+    localStorage.setItem("userGoogle", JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_LOGIN_GOOGLE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const logout = () => (dispatch) => {
-  localStorage.removeItem("userGoogle");
-  localStorage.removeItem("userInfo");
-  dispatch({ type: USER_LOGOUT_GOOGLE });
-  dispatch({ type: USER_LOGOUT });
-  dispatch({ type: CREATE_DIARY_RESET });
-};
+  localStorage.removeItem("userGoogle")
+  localStorage.removeItem("userInfo")
+  dispatch({ type: USER_LOGOUT_GOOGLE })
+  dispatch({ type: USER_LOGOUT })
+  dispatch({ type: CREATE_DIARY_RESET })
+}
 
 export const registerUser = (user) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_REGISTER_REQUEST,
-    });
+      type: USER_REGISTER_REQUEST
+    })
 
     const config = {
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
+        "Content-Type": "application/json"
+      }
+    }
 
-    const { data } = await axios.post("/api/users", user, config);
+    const { data } = await axios.post(
+      "https://diary-liv9.onrender.com/api/users",
+      user,
+      config
+    )
 
     dispatch({
       type: USER_REGISTER_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const loginUser = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_LOGIN_REQUEST,
-    });
+      type: USER_LOGIN_REQUEST
+    })
 
     const config = {
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
+        "Content-Type": "application/json"
+      }
+    }
 
     const { data } = await axios.post(
-      "/api/users/login",
+      "https://diary-liv9.onrender.com/api/users/login",
       { email, password },
       config
-    );
+    )
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
-      payload: data,
-    });
-    localStorage.setItem("userInfo", JSON.stringify(data));
+      payload: data
+    })
+    localStorage.setItem("userInfo", JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    });
+          : error.message
+    })
   }
-};
+}
 
 export const createBookmark = (id) => async (dispatch, getState) => {
   try {
-    dispatch({ type: CREATE_BOOKMARK_REQUEST }); // calls the DIARY_UPDATE_REQUEST reducer
+    dispatch({ type: CREATE_BOOKMARK_REQUEST }) // calls the DIARY_UPDATE_REQUEST reducer
 
     const {
       userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
     const userId = `${
       (userGoogle && userGoogle._id) || (userInfo && userInfo._id)
-    }`;
+    }`
 
     const { data } = await axios.post(
-      `/api/users/${userId}/bookmarks`,
+      `https://diary-liv9.onrender.com/api/users/${userId}/bookmarks`,
       { id },
       config
-    );
+    )
 
     dispatch({
       type: CREATE_BOOKMARK_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
   } catch (error) {
     dispatch({
       type: CREATE_BOOKMARK_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    }); // calls the DIARY_CREATE_REVIEW_FAIL reducer
+          : error.message
+    }) // calls the DIARY_CREATE_REVIEW_FAIL reducer
   }
-};
+}
 
 export const getBookmark = () => async (dispatch, getState) => {
   try {
-    dispatch({ type: GET_BOOKMARK_REQUEST }); // calls the DIARY_UPDATE_REQUEST reducer
+    dispatch({ type: GET_BOOKMARK_REQUEST }) // calls the DIARY_UPDATE_REQUEST reducer
 
     const {
       userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
     const userId = `${
       (userGoogle && userGoogle._id) || (userInfo && userInfo._id)
-    }`;
+    }`
 
-    const { data } = await axios.get(`/api/users/${userId}/bookmarks`, config);
+    const { data } = await axios.get(
+      `https://diary-liv9.onrender.com/api/users/${userId}/bookmarks`,
+      config
+    )
 
     dispatch({
       type: GET_BOOKMARK_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
   } catch (error) {
     dispatch({
       type: GET_BOOKMARK_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    }); // calls the GET_FAIL reducer
+          : error.message
+    }) // calls the GET_FAIL reducer
   }
-};
+}
 
 export const editUserById = (user) => async (dispatch, getState) => {
   try {
-    dispatch({ type: USER_EDIT_REQUEST });
+    dispatch({ type: USER_EDIT_REQUEST })
 
     const {
       userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
-    const { data } = await axios.put(`/api/users/`, user, config);
+    const { data } = await axios.put(
+      `https://diary-liv9.onrender.com/api/users/`,
+      user,
+      config
+    )
 
-    dispatch({ type: USER_EDIT_SUCCESS, payload: data });
+    dispatch({ type: USER_EDIT_SUCCESS, payload: data })
   } catch (error) {
     dispatch({
       type: USER_EDIT_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    }); // calls the GET_FAIL reducer
+          : error.message
+    }) // calls the GET_FAIL reducer
   }
-};
+}
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
   try {
-    dispatch({ type: GET_USER_DETAILS_REQUEST });
+    dispatch({ type: GET_USER_DETAILS_REQUEST })
 
     const {
       userLogin: { userInfo }, // we are destructuring two levels i.e getState().userLogin.userInfo
-      userLoginGoogle: { userGoogle },
-    } = getState();
+      userLoginGoogle: { userGoogle }
+    } = getState()
 
     const config = {
       headers: {
         Authorization: `Bearer ${
           (userGoogle && userGoogle.token) || (userInfo && userInfo.token)
-        }`,
-      },
-    };
+        }`
+      }
+    }
 
-    const { data } = await axios.get(`/api/users/${id}`, config);
+    const { data } = await axios.get(
+      `https://diary-liv9.onrender.com/api/users/${id}`,
+      config
+    )
 
-    console.log(data);
+    console.log(data)
 
     dispatch({
       type: GET_USER_DETAILS_SUCCESS,
-      payload: data,
-    });
+      payload: data
+    })
   } catch (error) {
     dispatch({
       type: GET_USER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.message,
-    }); // calls the GET_FAIL reducer
+          : error.message
+    }) // calls the GET_FAIL reducer
   }
-};
+}
